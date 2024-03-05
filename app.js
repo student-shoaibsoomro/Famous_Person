06f693f631ed72630fd119eb192bb814bc2f3d0b
student-shoaibsoomro <159219257+student-shoaibsoomro@users.noreply.github.com>
2024-03-05 05:03:27 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+let Author = "Albert Ienstein";
+let Quote = "A person who never made a mistake never tried anything new.";
+console.log(`${Author} once a said, ${Quote}`);
---
+++ app.ts
@@ -0,0 +1,3 @@
+let Author :string = "Albert Ienstein"
+let Quote :string = "A person who never made a mistake never tried anything new."
+console.log(`${Author} once a said, ${Quote}`);
